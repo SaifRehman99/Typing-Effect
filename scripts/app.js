1ae98ffaa9ae7b2de69cb604e9dd0c0d30ad5726
SaifRehman99 <saifr7493@gmail.com>
2019-05-24 20:47:39 +0500

    time speed fixed

--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,23 @@ class typingEffect {
     constructor(txtElement, wordsBundle, time) {
         this.txtElement = txtElement;
         this.wordsBundle = wordsBundle;
-        this.time = parseInt(time, 10);
+        this.time = time;
+
+        // to save the txt here
         this.text = '';
+        // index of array
         this.wordsBundleIndex = 0;
         this.deleteWord = false;
+        // function to set things
         this.func();
+
     }
 
     func() {
+
         // getting the index here
         const indexWords = this.wordsBundleIndex % this.wordsBundle.length;
+        
         // getting the text of bundle words
         const textBundle = this.wordsBundle[indexWords];
 
@@ -30,16 +37,11 @@ class typingEffect {
         this.txtElement.innerHTML = `<span class="innerTextCursor">${this.text}</span>`
 
         // setting the type speed here
-        let typeSpeed = 250;
-
-        // check if word is printed and ready to delete
-        if(this.deleteWord){
-            typeSpeed /=2;
-        }
+        let typeSpeed = 150;
 
 
         // check to see if the full word is printed so we can move on to the other
-        if(!this.deleteWord && this.text === textBundle){
+        if (!this.deleteWord && this.text === textBundle) {
             // to make the pause effect
             typeSpeed = this.time;
 
@@ -47,32 +49,40 @@ class typingEffect {
             this.deleteWord = true;
 
         }
-        else if(this.deleteWord && this.text === ''){
+        else if (this.deleteWord && this.text === '') {
             this.deleteWord = false;
 
             // moving to the next word
             this.wordsBundleIndex++;
 
             // pause effect
-            typeSpeed = 400;
+            typeSpeed = 300;
+        }
+
+         // check if word is printed and ready to delete
+         if (this.deleteWord) {
+            typeSpeed /= 2;
         }
 
         // calling the function here to go through the values
-        setTimeout(() => this.func(),typeSpeed);
+        setTimeout(() => this.func(), typeSpeed);
     }
 
 }
 
 // getting the things on the load 
-document.addEventListener('DOMContentLoaded', (start) => {
+document.addEventListener('DOMContentLoaded', () => {
+
     // gettimg the refrence here
+
+    // class of the span element
     const txtElement = document.querySelector('.typeEffect');
+    // array of words
     const wordsBundle = JSON.parse(txtElement.getAttribute('data-words'));
+    // time here
     const time = txtElement.getAttribute('data-wait');
 
+
     // initialize here
-
     new typingEffect(txtElement, wordsBundle, time);
-
-
 })
